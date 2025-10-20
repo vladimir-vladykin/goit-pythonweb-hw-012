c5c1f810189615e23ef349bc3ef025608cf747a3
Volodymyr Vladykin <vova@pocketguard.com>
2025-10-20 22:53:03 +0300

    Add documentation for ContactRepository; Setup sphinx

---
+++ docs/index.rst
@@ -0,0 +1,28 @@
+.. Contacts API documentation master file, created by
+   sphinx-quickstart on Mon Oct 20 22:28:35 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Contacts API documentation
+====================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+REST API repository Contacts
+====================================================
+
+.. automodule:: src.repository.contacts
+   :members:
+   :undoc-members:
+
+
+Indices and tables
+==========================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -8,19 +8,56 @@ from datetime import date, timedelta
 
 class ContactRepository:
     def __init__(self, session: AsyncSession):
+        """
+        Initialize a ContactRepository.
+
+        Args:
+            session: An AsyncSession object connected to the database.
+        """
         self.db = session
 
     async def get_contacts(self, skip: int, limit: int, user: User) -> List[Contact]:
+        """
+        Get a list of Contacts owned by `user` with pagination.
+
+        Args:
+            skip: The number of Contacts to skip.
+            limit: The maximum number of Contacts to return.
+            user: The owner of the Contacts to retrieve.
+
+        Returns:
+            A list of Contacts.
+        """
         stmt = select(Contact).filter_by(user=user).offset(skip).limit(limit)
         contacts = await self.db.execute(stmt)
         return contacts.scalars().all()
 
     async def get_contact_by_id(self, contact_id: int, user: User) -> Contact | None:
+        """
+        Get a Contact by its id.
+
+        Args:
+            contact_id: The id of the Contact to retrieve.
+            user: The owner of the Contact to retrieve.
+
+        Returns:
+            The Contact with the specified id, or None if no such Contact exists.
+        """
         stmt = select(Contact).filter_by(id=contact_id, user=user)
         contact = await self.db.execute(stmt)
         return contact.scalar_one_or_none()
 
     async def create_contact(self, body: ContactModel, user: User) -> Contact:
+        """
+        Create a new Contact with the given attributes.
+
+        Args:
+            body: A ContactModel with the attributes to assign to the Contact.
+            user: The User who owns the Contact.
+
+        Returns:
+            A Contact with the assigned attributes.
+        """
         contact = Contact(**body.model_dump(exclude_unset=True), user=user)
         self.db.add(contact)
         await self.db.commit()
@@ -28,6 +65,16 @@ class ContactRepository:
         return await self.get_contact_by_id(contact.id, user)
 
     async def delete_contact(self, contact_id: int, user: User) -> Contact | None:
+        """
+        Delete a Contact by its id.
+
+        Args:
+            contact_id: The id of the Contact to delete.
+            user: The owner of the Contact to delete.
+
+        Returns:
+            The deleted Contact, or None if no Contact with the given id exists.
+        """
         contact = await self.get_contact_by_id(contact_id, user)
         if contact:
             await self.db.delete(contact)
@@ -37,6 +84,17 @@ class ContactRepository:
     async def update_contact(
         self, contact_id: int, body: ContactModel, user: User
     ) -> Contact | None:
+        """
+        Update a Contact with the given attributes.
+
+        Args:
+            contact_id: The id of the Contacts to update.
+            body: A ContactModel with the attributes to assign to the Contacts.
+            user: The User who owns the Contact.
+
+        Returns:
+            The updated Contact, or None if no Contacts with the given id exists.
+        """
         contact = await self.get_contact_by_id(contact_id, user)
         if contact:
             for key, value in body.model_dump(exclude_unset=True).items():
@@ -55,6 +113,20 @@ class ContactRepository:
         limit: int,
         user: User,
     ) -> List[Contact]:
+        """
+        Get a list of Contacts owned by `user` with pagination, considering filter params
+
+        Args:
+            first_name: Optional filter by first name
+            last_name: Optional filter by second name
+            email: Optional filter by user's email
+            skip: The number of Contacts to skip.
+            limit: The maximum number of Contacts to return.
+            user: The owner of the Contacts to retrieve.
+
+        Returns:
+            A list of Contacts, filtered by params
+        """
         stmt = select(Contact).filter_by(user=user).offset(skip).limit(limit)
         if first_name:
             stmt = stmt.filter_by(first_name=first_name)
@@ -66,6 +138,15 @@ class ContactRepository:
         return contacts.scalars().all()
 
     async def get_closest_brithday_contacts(self, user: User) -> List[Contact]:
+        """
+        Get a list of Contacts with birthday within next 7 days.
+
+        Args:
+            user: The owner of the Contacts to retrieve.
+
+        Returns:
+            A list of Contacts.
+        """
         today = date.today()
         week_from_now = today + timedelta(days=7)
 
